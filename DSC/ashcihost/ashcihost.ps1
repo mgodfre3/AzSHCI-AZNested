cc8410e59fcdae82984e30730a598a01efae5358
mgodfre3 <migodfre@microsoft.com>
2021-02-24 09:30:23 -0500

    redid DSC config-simplified for testing

--- DSC/ashcihost/ashcihost.ps1
+++ DSC/ashcihost/ashcihost.ps1
@@ -1,16 +1,30 @@
-Configuration ASHCIHost {
-Param()
+Configuration Hypervisor {
 
-
-Import-DscResource -ModuleName "PSDesiredStateConfiguration"
-
-Node 'localhost'{
-        WindowsFeature Hyper-V {
-        Name = "Hyper-V" 
-        Ensure = "Present"
-        }
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    
+    Node Hypervisor{
+    
+    #Windows Features Installations
+    WindowsFeature Hyper-V {
+    Ensure = 'Present'
+    Name = "Hyper-V"
+    IncludeAllSubFeature = $true
     }
-
-
-
-}
+    
+    WindowsFeature Hyper-V-PowerShell{
+    Ensure = 'Present'
+    Name='Hyper-V-PowerShell'
+    IncludeAllSubFeature = $true
+    }
+    
+    
+    }
+    
+    
+    
+    }
+    
+    Hypervisor 
+    
+    
+    
