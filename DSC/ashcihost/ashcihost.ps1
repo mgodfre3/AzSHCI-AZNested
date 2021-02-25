091f534ef484151291190a479fc0a8d64d7e7d08
mgodfre3 <migodfre@microsoft.com>
2021-02-25 09:17:41 -0500

    added various quality settings to DSC config

--- DSC/ashcihost/ashcihost.ps1
+++ DSC/ashcihost/ashcihost.ps1
@@ -1,12 +1,12 @@
 Configuration ASHCIHost {
 
     param(
-    [Parameter(Mandatory)]
+    #[Parameter(Mandatory)]
     [System.Management.Automation.PSCredential]$Admincreds,
     [String]$targetDrive = "V",
     [String]$targetVMPath = "$targetDrive" + ":\VMs",
     [String]$dsc_source="https://raw.githubusercontent.com/billcurtis/AzSHCISandbox/main/",
-    [Parameter(Mandatory)]
+    #[Parameter(Mandatory)]
     [string]$customRdpPort,
     [String]$ashci_uri="https://ashcinested.blob.core.windows.net/vhd/AzStackHCIPreview1.vhdx?sp=r&st=2021-02-25T02:10:02Z&se=2021-03-31T09:10:02Z&spr=https&sv=2020-02-10&sr=b&sig=HmkDCro8ezz1tdf7VO%2FMAq8yd8HwOv%2B%2BiaBvGlSOBoA%3D"
 
@@ -14,6 +14,7 @@ Configuration ASHCIHost {
     
     Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
     Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
+    Import-DscResource -ModuleName 'xCredSSP'
 
     
     Node localhost{
@@ -103,15 +104,174 @@ Configuration ASHCIHost {
         }
         DependsOn  = "[Script]VirtualDisk"
     }
-    
-    
 
+    #### STAGE- SET WINDOWS DEFENDER EXCLUSION FOR VM STORAGE ####
 
+    Script defenderExclusions {
+        SetScript  = {
+            $exclusionPath = "$Using:targetDrive" + ":\"
+            Add-MpPreference -ExclusionPath "$exclusionPath"               
+        }
+        TestScript = {
+            $exclusionPath = "$Using:targetDrive" + ":\"
+            (Get-MpPreference).ExclusionPath -contains "$exclusionPath"
+        }
+        GetScript  = {
+            $exclusionPath = "$Using:targetDrive" + ":\"
+            @{Ensure = if ((Get-MpPreference).ExclusionPath -contains "$exclusionPath") { 'Present' } Else { 'Absent' } }
+        }
+        DependsOn  = "[File]VMfolder"
+    }
 
+    #### STAGE 1c - REGISTRY & SCHEDULED TASK TWEAKS ####
 
+    Registry "Disable Internet Explorer ESC for Admin" {
+        Key       = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+        Ensure    = 'Present'
+        ValueName = "IsInstalled"
+        ValueData = "0"
+        ValueType = "Dword"
+    }
 
+    Registry "Disable Internet Explorer ESC for User" {
+        Key       = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+        Ensure    = 'Present'
+        ValueName = "IsInstalled"
+        ValueData = "0"
+        ValueType = "Dword"
     }
     
-}   
+    Registry "Disable Server Manager WAC Prompt" {
+        Key       = "HKLM:\SOFTWARE\Microsoft\ServerManager"
+        Ensure    = 'Present'
+        ValueName = "DoNotPopWACConsoleAtSMLaunch"
+        ValueData = "1"
+        ValueType = "Dword"
+    }
+
+    Registry "Disable Network Profile Prompt" {
+        Key       = 'HKLM:\System\CurrentControlSet\Control\Network\NewNetworkWindowOff'
+        Ensure    = 'Present'
+        ValueName = ''
+    }
+
+    if ($environment -eq "Workgroup") {
+        Registry "Set Network Private Profile Default" {
+            Key       = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\010103000F0000F0010000000F0000F0C967A3643C3AD745950DA7859209176EF5B87C875FA20DF21951640E807D7C24'
+            Ensure    = 'Present'
+            ValueName = "Category"
+            ValueData = "1"
+            ValueType = "Dword"
+        }
+
+        Registry "SetWorkgroupDomain" {
+            Key       = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"
+            Ensure    = 'Present'
+            ValueName = "Domain"
+            ValueData = "$DomainName"
+            ValueType = "String"
+        }
+
+        Registry "SetWorkgroupNVDomain" {
+            Key       = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"
+            Ensure    = 'Present'
+            ValueName = "NV Domain"
+            ValueData = "$DomainName"
+            ValueType = "String"
+        }
+
+        Registry "NewCredSSPKey" {
+            Key       = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly'
+            Ensure    = 'Present'
+            ValueName = ''
+        }
+
+        Registry "NewCredSSPKey2" {
+            Key       = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation'
+            ValueName = 'AllowFreshCredentialsWhenNTLMOnly'
+            ValueData = '1'
+            ValueType = "Dword"
+            DependsOn = "[Registry]NewCredSSPKey"
+        }
+
+        Registry "NewCredSSPKey3" {
+            Key       = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly'
+            ValueName = '1'
+            ValueData = "*.$DomainName"
+            ValueType = "String"
+            DependsOn = "[Registry]NewCredSSPKey2"
+        }
+    }
+
+    ScheduledTask "Disable Server Manager at Startup" {
+        TaskName = 'ServerManager'
+        Enable   = $false
+        TaskPath = '\Microsoft\Windows\Server Manager'
+    }
+
+    #### STAGE 1d - CUSTOM FIREWALL BASED ON ARM TEMPLATE ####
+
+    if ($customRdpPort -ne "3389") {
+
+        Registry "Set Custom RDP Port" {
+            Key       = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp'
+            ValueName = "PortNumber"
+            ValueData = "$customRdpPort"
+            ValueType = 'Dword'
+        }
+    
+        Firewall AddFirewallRule {
+            Name        = 'CustomRdpRule'
+            DisplayName = 'Custom Rule for RDP'
+            Ensure      = 'Present'
+            Enabled     = 'True'
+            Profile     = 'Any'
+            Direction   = 'Inbound'
+            LocalPort   = "$customRdpPort"
+            Protocol    = 'TCP'
+            Description = 'Firewall Rule for Custom RDP Port'
+        }
+    }
+    #### STAGE 2h - CONFIGURE CREDSSP & WinRM
+
+    xCredSSP Server {
+        Ensure         = "Present"
+        Role           = "Server"
+        DependsOn      = "[DnsConnectionSuffix]AddSpecificSuffixNATNic"
+        SuppressReboot = $true
+    }
+    xCredSSP Client {
+        Ensure            = "Present"
+        Role              = "Client"
+        DelegateComputers = "$env:COMPUTERNAME" + ".$DomainName"
+        DependsOn         = "[xCredSSP]Server"
+        SuppressReboot    = $true
+    }
+
+    #### STAGE 3a - CONFIGURE WinRM
+
+    Script ConfigureWinRM {
+        SetScript  = {
+            Set-Item WSMan:\localhost\Client\TrustedHosts "*.$Using:DomainName" -Force
+        }
+        TestScript = {
+            (Get-Item WSMan:\localhost\Client\TrustedHosts).Value -contains "*.$Using:DomainName"
+        }
+        GetScript  = {
+            @{Ensure = if ((Get-Item WSMan:\localhost\Client\TrustedHosts).Value -contains "*.$Using:DomainName") { 'Present' } Else { 'Absent' } }
+        }
+        DependsOn  = "[xCredSSP]Client"
+    }
+}
+    
+    
+
+
+
+
+
+}
+    
+ 
     
     
